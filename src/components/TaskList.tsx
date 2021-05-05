8eba7af5801754d8adef3380c4103abb2555d168
caduxl007 <eduardo2020dev@gmail.com>
2021-05-04 21:59:23 -0300

    challenge completed

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,33 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) {
+      alert('Campo vazio não permetido!');
+      return;
+    }
+
+    const newTask = {
+      id: Math.random() * (99999 - 0) + 0,
+      title: newTaskTitle,
+      isComplete: false,
+    }
+
+    setTasks([...tasks, newTask]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    setTasks(oldValue => 
+      [...oldValue.map((task) => task.id === id 
+        ? {...task, isComplete: !task.isComplete} 
+        : task)
+      ]);
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    setTasks(oldValue => oldValue.filter((task) => task.id !== id));
+
+    alert('Task removida com sucesso!');
   }
 
   return (
